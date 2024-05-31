64c12ed0eec73c4b0d5609a2cd0f3c1255511f08
Gaston Acosta <gasteac@gmail.com>
2024-05-31 18:50:41 -0300

    chore: Update SignUp.jsx to apply smaller font size to error message

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -74,7 +74,7 @@ export const SignUp = () => {
                 d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
               />
             </svg>
-            <span>{signUpErrorMsg}</span>
+            <span className="text-sm">{signUpErrorMsg}</span>
           </div>
         ) : null}
         <label className="input input-bordered flex items-center gap-2">
